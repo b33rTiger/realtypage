1f56fb1727101cf4da51cf2d1358b5087998d74d
John <john@johnsheets.me>
2017-07-11 20:12:55 -0700

    started styling

--- src/proplist.jsx
+++ src/proplist.jsx
@@ -3,22 +3,22 @@ import './app.css';
 
 const PropList = (props) => {
   const listings = props.listings.map((item, index) =>
-    <div className="content col-md-6" key={index}>
-      <div className="image">
-        <img src={item.img} alt=""/>
+      <div className="content col-md-6 col-sm-12" key={index}>
+        <div className="image">
+          <img src={item.img} alt=""/>
+        </div>
+        <div className="listInfo">
+          <div className="street">{item.street}</div>
+          <div className="state">{item.city}, {item.state}</div>
+          <div className="price">{item.price}</div>
+        </div>
+        <ul className="features">
+          <li>{item.beds} beds</li>
+          <li>{item.baths} baths</li>
+          <li>{item.sqft} sq ft</li>
+        </ul>
+        <div className="year">{item.built}</div>
       </div>
-      <div className="listInfo">
-        <div className="street">{item.street}</div>
-        <div className="state">{item.state}</div>
-        <div className="price">{item.price}</div>
-      </div>
-      <ul className="features">
-        <li>{item.beds} bdrms</li>
-        <li>{item.baths} bath</li>
-        <li>{item.sqft} sq ft</li>
-      </ul>
-      <div className="year">{item.built}</div>
-    </div>
   );
   return (
     <div className="container">
