c6d42f327a05aa52929b66c46bd7b01d06d104ac
John <john@johnsheets.me>
2017-07-13 09:47:46 -0700

    styling almost 100%

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropList from './proplist';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { ButtonToolbar, Button } from 'react-bootstrap';
 import './app.css';
 
 class App extends Component {
@@ -15,10 +15,12 @@ class App extends Component {
     return (
       <div className="app">
         <div className="app-title">
-          <h1>Super Hero Listings Widget</h1>
-          <Button bsStyle="primary">Price</Button>
-          <Button bsStyle="primary">Beds</Button>
-          <Button bsStyle="primary">Sq. ft.</Button>
+          <h3>Super Hero Listings Widget</h3>
+          <div className="button-group">
+            <button type="button" className="btn btn-success">Price</button>
+            <button type="button" className="btn btn-success">Beds</button>
+            <button type="button" className="btn btn-success">Sq. ft.</button>
+          </div>
         </div>
         <PropList
           listings={this.props.listings}
--- src/proplist.jsx
+++ src/proplist.jsx
@@ -3,21 +3,25 @@ import './app.css';
 
 const PropList = (props) => {
   const listings = props.listings.map((item, index) =>
-      <div className="content col-md-6 col-sm-12" key={index}>
-        <div className="image">
+      <div className="row col-md-6 col-sm-12" key={index}>
+        <div className="content">
+          <div className="image">
           <img src={item.img} alt=""/>
+          </div>
+          <div className="listInfo">
+            <div className="street">{item.street}</div>
+            <div className="state">{item.city}, {item.state}</div>
+            <div className="price">{item.price}</div>
+            <div className="features">
+              <li>{item.beds} beds</li>
+              <li>{item.baths} baths</li>
+              {item.sqft > 0 && <li>{item.sqft} sq ft</li>}
+            </div>
+          </div>
+          <div className="year">
+            {item.built > 0 && <div>Built in {item.built}</div>}
+          </div>
         </div>
-        <div className="listInfo">
-          <div className="street">{item.street}</div>
-          <div className="state">{item.city}, {item.state}</div>
-          <div className="price">{item.price}</div>
-        </div>
-        <ul className="features">
-          <li>{item.beds} beds</li>
-          <li>{item.baths} baths</li>
-          <li>{item.sqft} sq ft</li>
-        </ul>
-        <div className="year">{item.built}</div>
       </div>
   );
   return (
