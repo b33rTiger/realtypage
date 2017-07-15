b50220c388cc6dd8184352eda89fc2670e50c3fa
John <john@johnsheets.me>
2017-07-14 18:35:21 -0700

    sorting code entered / almost working

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import PropList from './proplist';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 import './app.css';
@@ -7,9 +8,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      'sortedData': []
-    }
+      'filtered': []
+    };
+    this.handleClick = this.handleClick.bind(this);
   };
+
+  handleClick(value) {
+    const data = this.props.listings;
+    function sortData(value, data) {
+      return data.sort(function(a, b) {
+        let x = a[value];
+        let y = b[value];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      });
+    }
+    this.setState({
+      filtered: data
+    })
+  }
 
   render() {
     return (
@@ -17,13 +33,22 @@ class App extends Component {
         <div className="app-title">
           <h3>Super Hero Listings Widget</h3>
           <div className="button-group">
-            <button type="button" className="btn btn-success">Price</button>
-            <button type="button" className="btn btn-success">Beds</button>
+            <button
+              type="button"
+              className="btn btn-success">
+              Price
+              </button>
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => this.handleClick('beds')}>
+              Beds
+              </button>
             <button type="button" className="btn btn-success">Sq. ft.</button>
           </div>
         </div>
         <PropList
-          listings={this.props.listings}
+          listings={this.state.filtered.length === 0 ? this.props.listings : this.state.filtered}
         />
       </div>
     );
